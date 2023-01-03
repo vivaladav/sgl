38c7ea55798732f2b2b6fa142ed6bfb0e11792ab
Davide Coppola <vivaladav@gmail.com>
2023-01-03 10:07:05 -0500

    Add math constants to SGL core

---
+++ core/include/sgl/core/Math.h
@@ -0,0 +1,16 @@
+#pragma once
+
+namespace sgl
+{
+namespace core
+{
+
+class Math
+{
+public:
+    static const float PIf;
+    static const double PId;
+};
+
+} // namespace core
+} // namespace sgl
---
+++ core/src/Math.cpp
@@ -0,0 +1,11 @@
+#include "sgl/core/Math.h"
+
+namespace sgl
+{
+namespace core
+{
+const float Math::PIf = 3.14159265358979323846f;
+const double Math::PId = 3.14159265358979323846;
+
+} // namespace core
+} // namespace sgl
