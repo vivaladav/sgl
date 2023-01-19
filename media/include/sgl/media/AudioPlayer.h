d4a6270bd75fa60e793b63783a94fbbbfec589c4
Davide Coppola <vivaladav@gmail.com>
2023-01-19 22:49:48 +0100

    Allow to enable/disable music and sounds and control sounds play/stop

--- media/include/sgl/media/AudioPlayer.h
+++ media/include/sgl/media/AudioPlayer.h
@@ -20,8 +20,17 @@ public:
     void ResumeMusic();
     void StopMusic();
 
+    bool IsMusicEnabled() const;
+    void SetMusicEnabled(bool val);
+
     // -- SOUNDS --
     void PlaySound(const char * filename);
+    void PauseSounds();
+    void ResumeSounds();
+    void StopSounds();
+
+    bool IsSoundEnabled() const;
+    void SetSoundEnabled(bool val);
 
 private:
     AudioPlayer(AudioManager * am);
@@ -32,9 +41,15 @@ private:
 
     std::size_t mMusicPlayingId = 0;
 
+    bool mMusicEnabled = true;
+    bool mSoundEnabled = true;
+
     // only AudioManager can create and delete this
     friend class AudioManager;
 };
+
+inline bool AudioPlayer::IsMusicEnabled() const { return mMusicEnabled; }
+inline bool AudioPlayer::IsSoundEnabled() const { return mSoundEnabled; }
 
 inline AudioPlayer::AudioPlayer(AudioManager * am) : mAm(am) { }
 
--- media/src/AudioPlayer.cpp
+++ media/src/AudioPlayer.cpp
@@ -20,6 +20,10 @@ namespace media
 // -- MUSIC --
 void AudioPlayer::PlayMusic(const char * filename, bool restartSame)
 {
+    // do not play when music is disabled
+    if(!mMusicEnabled)
+        return ;
+
     const std::size_t musicId = mAm->GetFileId(filename);
 
     // music already playing and caller doesn't want to restart
@@ -38,26 +42,89 @@ void AudioPlayer::PlayMusic(const char * filename, bool restartSame)
 
 void AudioPlayer::PauseMusic()
 {
+    // do not pause when music is disabled
+    if(!mMusicEnabled)
+        return ;
+
     Mix_PauseMusic();
 }
 
 void AudioPlayer::ResumeMusic()
 {
+    // do not play when music is disabled
+    if(!mMusicEnabled)
+        return ;
+
     Mix_ResumeMusic();
 }
 
 void AudioPlayer::StopMusic()
 {
     Mix_HaltMusic();
+
+    mMusicPlayingId = 0;
+}
+
+void AudioPlayer::SetMusicEnabled(bool val)
+{
+    // nothing changed
+    if(val == mMusicEnabled)
+        return ;
+
+    mMusicEnabled = val;
+
+    // stop music if playing
+    if(mMusicEnabled && mMusicPlayingId != 0)
+        StopMusic();
 }
 
 // -- SOUNDS --
 void AudioPlayer::PlaySound(const char * filename)
 {
+    // do not play when sounds are disabled
+    if(!mSoundEnabled)
+        return ;
+
     Sound * sound = mAm->GetSound(filename);
 
     if(sound)
         sound->Play();
+}
+
+void AudioPlayer::PauseSounds()
+{
+    // do not pause when sounds are disabled
+    if(!mSoundEnabled)
+        return ;
+
+    Mix_Pause(-1);
+}
+
+void AudioPlayer::ResumeSounds()
+{
+    // do not resume when sounds are disabled
+    if(!mSoundEnabled)
+        return ;
+
+    Mix_Resume(-1);
+}
+
+void AudioPlayer::StopSounds()
+{
+    Mix_HaltChannel(-1);
+}
+
+void AudioPlayer::SetSoundEnabled(bool val)
+{
+    // nothing changed
+    if(val == mSoundEnabled)
+        return ;
+
+    mSoundEnabled = val;
+
+    // stop music if playing
+    if(mSoundEnabled)
+        StopSounds();
 }
 
 } // namespace media
