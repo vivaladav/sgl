8db914e593afaa87db72bb375696552b17d38a2a
Davide Coppola <vivaladav@gmail.com>
2026-07-17 12:01:28 +0200

    Do not update Widgets when not visible

--- sgui/src/WidgetContainer.cpp
+++ sgui/src/WidgetContainer.cpp
@@ -320,8 +320,8 @@ void WidgetContainer::PropagateUpdate(float delta)
 {
     for(Widget * w : mWidgets)
     {
-        // do not update if disabled
-        if(!w->IsEnabled())
+        // do not update if disabled or not visible
+        if(!w->IsEnabled() || !w->IsVisible() || !w->IsInVisibleArea())
             continue ;
 
         if(w->IsMouseOver())
