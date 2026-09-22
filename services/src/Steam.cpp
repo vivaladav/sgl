5995265b8beb761f844733caa3c05d8f5e3d2d67
Davide Coppola <vivaladav@gmail.com>
2026-09-22 13:08:25 +0200

    Fix Steam methods used when init is not done

--- services/src/Steam.cpp
+++ services/src/Steam.cpp
@@ -45,73 +45,113 @@ void Steam::Shutdown()
 {
     SteamAPI_Shutdown();
 }
+
 // -- APP INFO --
 unsigned int Steam::GetAppId()
 {
-    return SteamUtils()->GetAppID();
+    if(mInitDone)
+        return SteamUtils()->GetAppID();
+    else
+        return false;
 }
 
 const char * Steam::GetAppLanguage()
 {
-    return SteamApps()->GetCurrentGameLanguage();
+    if(mInitDone)
+        return SteamApps()->GetCurrentGameLanguage();
+    else
+        return nullptr;
 }
 
 // -- SYSTEM INFO --
 bool Steam::IsRunningOnDeck()
 {
-    return SteamUtils()->IsRunningOnSteamHardware() == k_ESteamHardwareTypeSteamDeck;
+    if(mInitDone)
+        return SteamUtils()->IsRunningOnSteamHardware() == k_ESteamHardwareTypeSteamDeck;
+    else
+        return false;
 }
 
 // -- USER INFO --
 unsigned int Steam::GetUserId()
 {
-    return SteamUser()->GetSteamID().GetAccountID();
+    if(mInitDone)
+        return SteamUser()->GetSteamID().GetAccountID();
+    else
+        return 0;
 }
 
 const char * Steam::GetUserName()
 {
-    return SteamFriends()->GetPersonaName();
+    if(mInitDone)
+        return SteamFriends()->GetPersonaName();
+    else
+        return nullptr;
 }
 
 // -- STATS --
 bool Steam::GetStat(const char * name, int * value)
 {
-    return SteamUserStats()->GetStat(name, value);
+    if(mInitDone)
+        return SteamUserStats()->GetStat(name, value);
+    else
+        return false;
 }
 
 bool Steam::GetStat(const char * name, float * value)
 {
-    return SteamUserStats()->GetStat(name, value);
+    if(mInitDone)
+        return SteamUserStats()->GetStat(name, value);
+    else
+        return false;
 }
 
 bool Steam::SetStat(const char * name, int value)
 {
-    return SteamUserStats()->SetStat(name, value);
+    if(mInitDone)
+        return SteamUserStats()->SetStat(name, value);
+    else
+        return false;
 }
 
 bool Steam::SetStat(const char * name, float value)
 {
-    return SteamUserStats()->SetStat(name, value);
+    if(mInitDone)
+        return SteamUserStats()->SetStat(name, value);
+    else
+        return false;
 }
 
 bool Steam::UpdateAverageStat(const char * name, float value, double sessionLen)
 {
-    return SteamUserStats()->UpdateAvgRateStat(name, value, sessionLen);
+    if(mInitDone)
+        return SteamUserStats()->UpdateAvgRateStat(name, value, sessionLen);
+    else
+        return false;
 }
 
 bool Steam::ResetStats()
 {
-    return SteamUserStats()->ResetAllStats(false);
+    if(mInitDone)
+        return SteamUserStats()->ResetAllStats(false);
+    else
+        return false;
 }
 
 // -- ACHIEVEMENTS --
 bool Steam::ClearAchievement(const char * name)
 {
-    return SteamUserStats()->ClearAchievement(name);
+    if(mInitDone)
+        return SteamUserStats()->ClearAchievement(name);
+    else
+        return false;
 }
 
 bool Steam::IsAchievementHidden(const char * name)
 {
+    if(!mInitDone)
+        return false;
+
     const std::string ok("1");
 
     const char * key = "hidden";
@@ -122,33 +162,51 @@ bool Steam::IsAchievementHidden(const char * name)
 
 bool Steam::IsAchievementUnlocked(const char * name, bool * unlocked)
 {
-    return SteamUserStats()->GetAchievement(name, unlocked);
+    if(mInitDone)
+        return SteamUserStats()->GetAchievement(name, unlocked);
+    else
+        return false;
 }
 
 unsigned int Steam::GetNumberOfAchievements()
 {
-    return SteamUserStats()->GetNumAchievements();
+    if(mInitDone)
+        return SteamUserStats()->GetNumAchievements();
+    else
+        return 0;
 }
 
 bool Steam::ShowAchievementProgressNotification(const char * name, unsigned int progress,
                                                 unsigned int max)
 {
-    return SteamUserStats()->IndicateAchievementProgress(name, progress, max);
+    if(mInitDone)
+        return SteamUserStats()->IndicateAchievementProgress(name, progress, max);
+    else
+        return false;
 }
 
 bool Steam::UnlockAchievement(const char * name)
 {
-    return SteamUserStats()->SetAchievement(name);
+    if(mInitDone)
+        return SteamUserStats()->SetAchievement(name);
+    else
+        return false;
 }
 
 bool Steam::ResetStatsAndAchievements()
 {
-    return SteamUserStats()->ResetAllStats(true);
+    if(mInitDone)
+        return SteamUserStats()->ResetAllStats(true);
+    else
+        return false;
 }
 
 bool Steam::StoreStatsAndAchievements()
 {
-    return SteamUserStats()->StoreStats();
+    if(mInitDone)
+        return SteamUserStats()->StoreStats();
+    else
+        return false;
 }
 
 // ===== NOT USING THE STEAM SDK =====
